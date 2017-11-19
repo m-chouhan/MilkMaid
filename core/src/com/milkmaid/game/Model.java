5fd22f2a25940e5ede0af19fdac1fd965c582ae4
m-chouhan <mchouhanofficial@gmail.com>
2017-11-19 18:41:37 +0530

    upgraded to java7

--- core/src/com/milkmaid/game/Model.java
+++ core/src/com/milkmaid/game/Model.java
@@ -11,5 +11,10 @@ public class Model {
     static final int WorldHeight= 480;
     static VertexQueue VQueue = new VertexQueue(25);
     public enum GameState {NORMAL,STRONGER,SHARPER}
+    public interface MyComparator {
+        public boolean compare(int a1, int a2);
+    }
 
+//    MyComparator myComparator = (a1, a2) -> {return a1 > a2;};
+//    boolean result = myComparator.compare(2, 5);
 }
